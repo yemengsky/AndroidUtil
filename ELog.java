2322ac13fa4298089e1d31de0472e8115e506c9c
yemengsky <yemengsky@gmail.com>
2015-09-11 14:31:18 +0800

    Update ELog.java

--- ELog.java
+++ ELog.java
@@ -1,16 +1,17 @@
 import android.util.Log;
 
+
 public class ELog {
-	// when online set this value to false
-	public static boolean debugeMode = BuildConfig.DEBUG;
-	// you can set this TAG
-	// notice that if you change it , all log will change to the tag you seted
-	public static String TAG = "MYLOG";
+    // when online set this value to false
+    public static boolean debugeMode = BuildConfig.DEBUG;
+    // you can set this TAG
+    // notice that if you change it , all log will change to the tag you seted
+    public static String TAG = "MYLOG";
 
-	private ELog() {
-	}
+    private ELog() {
+    }
 
- private static String getString(Object object) {
+    private static String getString(Object object) {
         StackTraceElement ste = new Throwable().getStackTrace()[2];
         String valueString = ste.getFileName() + " "
                 + ste.getMethodName() + ":" + ste.getLineNumber() + " "
@@ -18,21 +19,38 @@ public class ELog {
         return valueString;
     }
 
-	public static void e(Object object) {
-		if (debugeMode) {
-			Log.e(TAG, getString(object));
-		}
-	}
+    public static void e(Object object) {
+        if (debugeMode) {
+            Log.e(TAG, getString(object));
+        }
+    }
 
-	public static void i(Object object) {
-		if (debugeMode) {
-			Log.i(TAG, getString(object));
-		}
-	}
+    public static void i(Object object) {
+        if (debugeMode) {
+            Log.i(TAG, getString(object));
+        }
+    }
 
-	public static void d(Object object) {
-		if (debugeMode) {
-			Log.d(TAG, getString(object));
-		}
-	}
+    public static void d(Object object) {
+        if (debugeMode) {
+            Log.d(TAG, getString(object));
+        }
+    }
+    public static void e(String tag,Object object) {
+        if (debugeMode) {
+            Log.e(tag, getString(object));
+        }
+    }
+
+    public static void i(String tag,Object object) {
+        if (debugeMode) {
+            Log.i(tag, getString(object));
+        }
+    }
+
+    public static void d(String tag,Object object) {
+        if (debugeMode) {
+            Log.d(tag, getString(object));
+        }
+    }
 }
